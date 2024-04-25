33a6101f36f0c193bb69e1f8679611761155df47
Halum88 <114135252+Halum88@users.noreply.github.com>
2024-04-25 13:12:24 +0300

    Feature/db (#1)
    * create bot
    
    * create table in db
    
    * gitignore
    
    * del test
    
    * gitignore

---
+++ bot/database.py
@@ -0,0 +1,47 @@
+import psycopg2
+from psycopg2 import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def create_connection():
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        print("Connection to PostgreSQL DB successful")
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    return connection
+
+
+def create_messages_table(connection):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS messages (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER,
+                username VARCHAR(255),
+                text TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        connection.commit()
+        cursor.close()
+        print("Table 'messages' created successfully or already exists")
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating table 'messages':", error)
+
