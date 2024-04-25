10a82f882526bdf8df7a090549010606d6cde015
Halum88 <1234as4321@mail.ru>
2024-04-25 21:24:34 +0300

    create def read token in new file

---
+++ gpt/read_token.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+today = datetime.now().strftime("%Y-%m-%d")
+filename = f"tokenized_texts_{today}.txt"
+
+
+def read_tokenized_texts(file_path):
+    tokenized_texts = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            tokens = [int(token.strip()) for token in line.split(';')]
+            tokenized_texts.append(tokens)
+    return tokenized_texts
+
+if __name__ == "__main__":
+    file_path = f'./source/{filename}'
+    tokenized_texts = read_tokenized_texts(file_path)
+    print("Прочитанные токенизированные тексты:", tokenized_texts)
